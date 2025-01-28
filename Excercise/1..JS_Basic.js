fcdbac1ab539ce2a6b29b73514e0e6e636f19d4f
Bigyanhub <bigyanbasnet849@gmail.com>
2025-01-28 09:32:37 +0545

    Day 1

---
+++ Excercise/1..JS_Basic.js
@@ -0,0 +1,30 @@
+// 1a.
+alert('Good morning!');
+
+// 1b.
+alert('Bigyan');
+
+// 1c.
+10 + 5
+
+// 1d.
+20 - 5
+
+// 1e.
+2 + 2 - 5
+
+// 1f.
+document.body.innerHTML = 'Good morning!';
+
+// 1g.
+document.body.innerHTML = 'Bigyan';
+
+//1h.
+10 + 8 + 20
+
+// 1i.
+100 - 50 - 20 + 200
+
+// 1j.
+document.body.innerHTML = '';
+
